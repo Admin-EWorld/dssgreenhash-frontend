9c975b765e08b8786812fae3240a58eb8b2c3adc
Admin-EWorld <55896051+Admin-EWorld@users.noreply.github.com>
2025-04-03 23:32:44 +0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './screens/HomeScreen';
+import WithdrawScreen from './screens/WithdrawScreen';
+import SwapCoinScreen from './screens/SwapCoinScreen';
+import BoostScreen from './screens/BoostScreen';
+import ReferralScreen from './screens/ReferralScreen';
+import EarnScreen from './screens/EarnScreen';
+import MoreScreen from './screens/MoreScreen';
+import FAQScreen from './screens/FAQScreen';
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Withdraw" component={WithdrawScreen} />
+        <Stack.Screen name="SwapCoin" component={SwapCoinScreen} />
+        <Stack.Screen name="Boost" component={BoostScreen} />
+        <Stack.Screen name="Referral" component={ReferralScreen} />
+        <Stack.Screen name="Earn" component={EarnScreen} />
+        <Stack.Screen name="More" component={MoreScreen} />
+        <Stack.Screen name="FAQ" component={FAQScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
